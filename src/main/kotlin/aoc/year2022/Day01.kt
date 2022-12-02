44eff9508acde6ae7863a672beefd9494eb83d90
Juan José González-Abril <juanjo@gonzabril.es>
2022-12-02 21:58:53 +0100

    Solve Day 1 AoC 2022

---
+++ src/main/kotlin/aoc/year2022/Day01.kt
@@ -0,0 +1,16 @@
+package aoc.year2022
+
+import aoc.Puzzle
+
+/**
+ * [Day 1 - Advent of Code 2022](https://adventofcode.com/2022/day/1)
+ */
+object Day01 : Puzzle<Int, Int> {
+    override fun solvePartOne(input: String): Int =
+        input.parse().maxOf { it.lines().sumOf(String::toInt) }
+
+    override fun solvePartTwo(input: String): Int =
+        input.parse().map { it.lines().sumOf(String::toInt) }.sortedDescending().take(3).sum()
+
+    private fun String.parse() = split("\n\n")
+}
--- src/test/kotlin/aoc/PuzzleTest.kt
+++ src/test/kotlin/aoc/PuzzleTest.kt
@@ -1,12 +1,15 @@
 package aoc
 
+import aoc.year2022.Day01
 import org.junit.jupiter.api.DynamicContainer.dynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
 
 internal class PuzzleTest {
-    private val puzzles: List<TestCase<*, *>> = listOf()
+    private val puzzles: List<TestCase<*, *>> = listOf(
+        TestCase(Day01, answer1 = 65912, answer2 = 195625)
+    )
 
     @TestFactory
     internal fun puzzles(): List<DynamicNode> = puzzles.map { testCase ->
