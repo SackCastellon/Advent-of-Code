0d2041092a51b43f8d2d9b71dc5a0d5fbe67cead
Juan José González Abril <juanjo@gonzabril.es>
2023-12-01 12:54:21 +0100

    Solve Day 1 AoC 2023

---
+++ src/main/kotlin/aoc/year2023/Day01.kt
@@ -0,0 +1,50 @@
+package aoc.year2023
+
+import aoc.Puzzle
+
+/**
+ * [Day 1 - Advent of Code 2023](https://adventofcode.com/2023/day/1)
+ */
+object Day01 : Puzzle<Int, Int> {
+    private val digitSpellings = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+    )
+
+    override fun solvePartOne(input: String): Int =
+        input.lineSequence().filterNot(String::isBlank).sumOf(::getCalibrationValue)
+
+    override fun solvePartTwo(input: String): Int =
+        input.lineSequence().filterNot(String::isBlank).map(::processSpelling).sumOf(::getCalibrationValue)
+
+    private fun getCalibrationValue(string: String): Int {
+        val firstDigit = string.find(Char::isDigit)!!.digitToInt()
+        val lastDigit = string.findLast(Char::isDigit)!!.digitToInt()
+        return (firstDigit * 10) + lastDigit
+    }
+
+    private fun processSpelling(string: String): String = string.let(::replaceFirstSpelling).let(::replaceLastSpelling)
+
+    private fun replaceFirstSpelling(string: String): String {
+        return digitSpellings.mapValues { string.indexOf(it.key) }
+            .filter { (_, v) -> v >= 0 }
+            .minByOrNull { it.value }
+            ?.let { string.replaceFirst(it.key, digitSpellings.getValue(it.key)) }
+            ?: string
+    }
+
+    private fun replaceLastSpelling(string: String): String {
+        return digitSpellings.mapValues { string.lastIndexOf(it.key) }
+            .filter { (_, v) -> v >= 0 }
+            .maxByOrNull { it.value }
+            ?.let { string.replace(it.key, digitSpellings.getValue(it.key)) }
+            ?: string
+    }
+}
--- src/test/kotlin/aoc/PuzzleTest.kt
+++ src/test/kotlin/aoc/PuzzleTest.kt
@@ -1,6 +1,5 @@
 package aoc
 
-import aoc.year2022.*
 import org.junit.jupiter.api.DynamicContainer.dynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest.dynamicTest
@@ -8,13 +7,17 @@ import org.junit.jupiter.api.TestFactory
 
 internal class PuzzleTest {
     private val puzzles: List<TestCase<*, *>> = listOf(
-        TestCase(Day01, answer1 = 65912, answer2 = 195625),
-        TestCase(Day02, answer1 = 12645, answer2 = 11756),
-        TestCase(Day03, answer1 = 7766, answer2 = 2415),
-        TestCase(Day04, answer1 = 644, answer2 = 926),
-        TestCase(Day05, answer1 = "ZRLJGSCTR", answer2 = "PRTTGRFPB"),
-        TestCase(Day06, answer1 = 1109, answer2 = 3965),
-        TestCase(Day08, answer1 = 1669, answer2 = 0),
+        // Year 2022
+        TestCase(aoc.year2022.Day01, answer1 = 65912, answer2 = 195625),
+        TestCase(aoc.year2022.Day02, answer1 = 12645, answer2 = 11756),
+        TestCase(aoc.year2022.Day03, answer1 = 7766, answer2 = 2415),
+        TestCase(aoc.year2022.Day04, answer1 = 644, answer2 = 926),
+        TestCase(aoc.year2022.Day05, answer1 = "ZRLJGSCTR", answer2 = "PRTTGRFPB"),
+        TestCase(aoc.year2022.Day06, answer1 = 1109, answer2 = 3965),
+        TestCase(aoc.year2022.Day08, answer1 = 1669, answer2 = 0),
+
+        // Year 2023
+        TestCase(aoc.year2023.Day01, answer1 = 54597, answer2 = 54517),
     )
 
     @TestFactory
