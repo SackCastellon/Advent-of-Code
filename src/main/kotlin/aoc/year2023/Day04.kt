012f84d3b0d3ed19cd63c773f3a0ad6a06bfc250
Juan José González Abril <juanjo@gonzabril.es>
2023-12-04 17:08:02 +0100

    refactor: Simplify solution 2023 day 4 part 1

--- src/main/kotlin/aoc/year2023/Day04.kt
+++ src/main/kotlin/aoc/year2023/Day04.kt
@@ -7,18 +7,19 @@ import kotlin.math.pow
  * [Day 4 - Advent of Code 2023](https://adventofcode.com/2023/day/4)
  */
 object Day04 : Puzzle<Int, Int> {
-    override fun solvePartOne(input: String): Int = input.lineSequence()
-        .map { it.substringAfter(':') }
-        .map { parseCard(it) }
-        .map { (winning, actual) -> actual.count { it in winning } }
-        .map { if (it == 1) 1 else 2.0.pow(it - 1.0).toInt() }
-        .sum()
-
-    private fun parseCard(card: String): Pair<Set<Int>, Set<Int>> {
-        return card.split('|')
-            .map { it.splitToSequence(' ').filter { it.isNotBlank() }.map { it.toInt() }.toSet() }
-            .let { (a, b) -> a to b }
-    }
+    override fun solvePartOne(input: String): Int = input.lineSequence().map { it.toCard() }.sumOf { it.points }
 
     override fun solvePartTwo(input: String) = TODO()
+}
+
+private fun String.toCard(): Card {
+    val (idStr, winningStr, playingStr) = split(':', '|').map { it.trim() }
+    val id = idStr.removePrefix("Card ").trim().toInt()
+    val winning = winningStr.splitToSequence(' ').filter { it.isNotEmpty() }.map { it.toInt() }.toSet()
+    val playing = playingStr.splitToSequence(' ').filter { it.isNotEmpty() }.map { it.toInt() }.toSet()
+    return Card(id, winning, playing)
+}
+
+private data class Card(val id: Int, val winning: Set<Int>, val playing: Set<Int>) {
+    val points = playing.count { it in winning }.let { if (it == 0) 0 else 2.0.pow(it - 1.0).toInt() }
 }
