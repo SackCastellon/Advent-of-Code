701b917ef3b2fd92c2d084b9024536b591ab204f
Juan José González Abril <juanjo@gonzabril.es>
2023-12-13 10:10:48 +0100

    feat: Solve 2023 day 9 part 2

--- src/main/kotlin/aoc/year2023/Day09.kt
+++ src/main/kotlin/aoc/year2023/Day09.kt
@@ -6,14 +6,19 @@ import aoc.Puzzle
  * [Day 9 - Advent of Code 2023](https://adventofcode.com/2023/day/9)
  */
 object Day09 : Puzzle<Int, Int> {
-    override fun solvePartOne(input: String): Int = input.lineSequence()
-        .map { it.split(' ').map(String::toInt) }
-        .sumOf(::getNextValue)
+    override fun solvePartOne(input: String): Int = input.lineSequence().map(::processLine).sumOf(::getNextValue)
 
-    override fun solvePartTwo(input: String): Int = TODO()
+    override fun solvePartTwo(input: String): Int = input.lineSequence().map(::processLine).sumOf(::getPreviousValue)
+
+    private fun processLine(it: String) = it.split(' ').map(String::toInt)
 
     private fun getNextValue(values: List<Int>): Int {
         if (values.all { it == 0 }) return 0
         return values.last() + getNextValue(values.zipWithNext().map { (a, b) -> b - a })
+    }
+
+    private fun getPreviousValue(values: List<Int>): Int {
+        if (values.all { it == 0 }) return 0
+        return values.first() - getPreviousValue(values.zipWithNext().map { (a, b) -> b - a })
     }
 }
