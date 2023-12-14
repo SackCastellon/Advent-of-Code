5be7a573533ea427dea7cac171276db6659d07b0
Juan José González Abril <juanjo@gonzabril.es>
2023-12-14 16:30:42 +0100

    refactor: extract common code to function, broaden parameter and return types

--- src/main/kotlin/aoc/year2023/Day09.kt
+++ src/main/kotlin/aoc/year2023/Day09.kt
@@ -6,18 +6,18 @@ import aoc.Puzzle
  * [Day 9 - Advent of Code 2023](https://adventofcode.com/2023/day/9)
  */
 object Day09 : Puzzle<Int, Int> {
-    override fun solvePartOne(input: String): Int = input.lineSequence().map(::processLine).sumOf(::getNextValue)
+    override fun solvePartOne(input: String): Int = input.process().sumOf(::getNextValue)
 
-    override fun solvePartTwo(input: String): Int = input.lineSequence().map(::processLine).sumOf(::getPreviousValue)
+    override fun solvePartTwo(input: String): Int = input.process().sumOf(::getPreviousValue)
 
-    private fun processLine(it: String) = it.split(' ').map(String::toInt)
+    private fun String.process(): Sequence<Iterable<Int>> = lineSequence().map { it.split(' ').map(String::toInt) }
 
-    private fun getNextValue(values: List<Int>): Int {
+    private fun getNextValue(values: Iterable<Int>): Int {
         if (values.all { it == 0 }) return 0
         return values.last() + getNextValue(values.zipWithNext().map { (a, b) -> b - a })
     }
 
-    private fun getPreviousValue(values: List<Int>): Int {
+    private fun getPreviousValue(values: Iterable<Int>): Int {
         if (values.all { it == 0 }) return 0
         return values.first() - getPreviousValue(values.zipWithNext().map { (a, b) -> b - a })
     }
