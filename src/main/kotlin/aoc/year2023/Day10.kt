1e80204984e6a83f44bf08d26bc6da33735d128e
Juan José González Abril <juanjo@gonzabril.es>
2023-12-15 12:47:36 +0100

    feat: Solve 2023 day 10 part 2

--- src/main/kotlin/aoc/year2023/Day10.kt
+++ src/main/kotlin/aoc/year2023/Day10.kt
@@ -7,9 +7,35 @@ import aoc.Puzzle
  */
 object Day10 : Puzzle<Int, Int> {
     override fun solvePartOne(input: String): Int {
-        lateinit var start: Point
+        val (connectionMap, start) = getConnectionMap(input)
 
-        val connections = input.lines()
+        return connectionMap.getValue(start)
+            .filter { start in connectionMap.getValue(it) }
+            .map { getPath(start to it, connectionMap) }
+            .let { (a, b) -> a zip b }
+            .takeWhile { (a, b) -> a != b }
+            .count() + 1
+    }
+
+    override fun solvePartTwo(input: String): Int {
+        val (connectionMap, start) = getConnectionMap(input)
+
+        val loopPoints = connectionMap.getValue(start)
+            .first { start in connectionMap.getValue(it) }
+            .let { getPath(start to it, connectionMap) }
+            .takeWhile { it != start }
+            .toSet() + start
+
+        val loopConnectionMap = connectionMap.filterKeys { it in loopPoints }.withDefault { listOf() }
+
+        val cleanField = input.cleanField(loopConnectionMap)
+
+        return cleanField.sumOf { it.getContainedTiles() }
+    }
+
+    private fun getConnectionMap(input: String): Pair<Map<Point, List<Point>>, Point> {
+        lateinit var start: Point
+        return input.lines()
             .flatMapIndexed { y, line ->
                 line.mapIndexed { x, pipe ->
                     val p = Point(x, y)
@@ -28,29 +54,85 @@ object Day10 : Puzzle<Int, Int> {
             }
             .toMap()
             .withDefault { listOf() }
-
-        fun getPathThrough(point: Point): Sequence<Point> {
-            return generateSequence(start to point) { (prev, curr) ->
-                connections.getValue(curr)
-                    .single { it != prev && curr in connections.getValue(it) }
-                    .let { curr to it }
-            }.map { it.second }
-        }
-
-        return connections.getValue(start)
-            .filter { start in connections.getValue(it) }
-            .map(::getPathThrough)
-            .let { (a, b) -> a zip b }
-            .takeWhile { (a, b) -> a != b }
-            .count() + 1
+            .let { it to start }
     }
 
-    override fun solvePartTwo(input: String): Int = TODO()
+    private fun String.cleanField(connectionMap: Map<Point, List<Point>>): List<List<Char>> {
+        return lines().mapIndexed { y, line ->
+            line.mapIndexed { x, pipe ->
+                val point = Point(x, y)
+                when (val size = connectionMap.getValue(point).size) {
+                    0 -> '.'
+                    2 -> pipe
+                    4 -> {
+                        point.getPipe(connectionMap)
+                    }
 
-    private data class Point(val x: Int, val y: Int) {
+                    else -> error("Unexpected number of connections $size")
+                }
+            }
+        }
+    }
+
+    private fun Point.getPipe(connectionMap: Map<Point, List<Point>>): Char {
+        val startConnections = connectionMap.getValue(this)
+            .filter { connectionMap.getValue(it).any(::equals) }
+            .toSortedSet()
+
+        return when (startConnections) {
+            sortedSetOf(north(), south()) -> '|'
+            sortedSetOf(east(), west()) -> '-'
+            sortedSetOf(north(), east()) -> 'L'
+            sortedSetOf(north(), west()) -> 'J'
+            sortedSetOf(south(), west()) -> '7'
+            sortedSetOf(south(), east()) -> 'F'
+            else -> error(startConnections)
+        }
+    }
+
+    private fun List<Char>.getContainedTiles(): Int {
+        var inside = false
+        var lastCorner: Char? = null
+        var count = 0
+        for (pipe in this) {
+            if (pipe in setOf('|', 'L', 'J', '7', 'F')) inside = !inside
+
+            when (pipe) {
+                'L', 'F' -> lastCorner = pipe
+                '7' -> {
+                    if (lastCorner == 'L') inside = !inside
+                    lastCorner = null
+                }
+
+                'J' -> {
+                    if (lastCorner == 'F') inside = !inside
+                    lastCorner = null
+                }
+            }
+
+            if (pipe == '.' && inside) count++
+        }
+        return count
+    }
+
+    private fun getPath(direction: Pair<Point, Point>, connections: Map<Point, List<Point>>): Sequence<Point> {
+        return generateSequence(direction) { (prev, curr) ->
+            connections.getValue(curr)
+                .single { it != prev && curr in connections.getValue(it) }
+                .let { curr to it }
+        }.map { it.second }
+    }
+
+    private data class Point(val x: Int, val y: Int) : Comparable<Point> {
         fun north() = copy(y = y - 1)
         fun south() = copy(y = y + 1)
         fun east() = copy(x = x + 1)
         fun west() = copy(x = x - 1)
+
+        override fun compareTo(other: Point): Int = comparator.compare(this, other)
+
+        companion object {
+            private val comparator = Comparator.comparingInt(Point::x).thenComparingInt(Point::y)
+        }
     }
 }
