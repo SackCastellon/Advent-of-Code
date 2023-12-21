dce49e37247179152ec0304954bd3eb54682b1ef
Juan José González Abril <juanjo@gonzabril.es>
2023-12-21 14:33:25 +0100

    feat: Solve 2023 day 13 part 2

--- src/main/kotlin/aoc/year2023/Day13.kt
+++ src/main/kotlin/aoc/year2023/Day13.kt
@@ -16,14 +16,29 @@ object Day13 : Puzzle<Int, Int> {
             linesAbove?.let { it * 100 } ?: linesLeft!!
         }
 
-    override fun solvePartTwo(input: String): Int = TODO()
+    override fun solvePartTwo(input: String): Int = input.split("\n\n")
+        .map { it.lines().map(String::toList) }
+        .sumOf { rows ->
+            val linesLeft = getLineOfReflectionWithOneChange(rows.transposed())
+            val linesAbove = getLineOfReflectionWithOneChange(rows)
+            linesLeft ?: (linesAbove!! * 100)
+        }
 
     private fun getLineOfReflection(input: List<List<Char>>): Int? =
         (1..input.lastIndex).firstNotNullOfOrNull { index ->
             val size = min(index, input.size - index)
             val firstHalf = input.take(index).takeLast(size)
-            val secondHalf = input.drop(index).take(size)
-            index.takeIf { firstHalf == secondHalf.asReversed() }
+            val secondHalf = input.drop(index).take(size).asReversed()
+            index.takeIf { firstHalf == secondHalf }
+        }
+
+    private fun getLineOfReflectionWithOneChange(input: List<List<Char>>): Int? =
+        (1..input.lastIndex).firstNotNullOfOrNull { index ->
+            val size = min(index, input.size - index)
+            val firstHalf = input.take(index).takeLast(size)
+            val secondHalf = input.drop(index).take(size).asReversed()
+            val changesNeeded = firstHalf.zip(secondHalf).sumOf { (a, b) -> a.zip(b).count { (a, b) -> a != b } }
+            index.takeIf { changesNeeded == 1 }
         }
 
     private fun <T> List<List<T>>.transposed(): List<List<T>> {
